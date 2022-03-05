fb2cdc41b5c558bc8150c16d480e37efe397e0e4
Анна Маршалова <78635473+anna-marshalova@users.noreply.github.com>
2022-03-05 22:08:06 +0700

    Add files via upload

--- recipe.cxx
+++ recipe.cxx
@@ -1,25 +1,27 @@
-#encoding "utf-8"   // сообщаем парсеру о том, в какой кодировке написана грамматика
-#GRAMMAR_ROOT S      // указываем корневой нетерминал грамматики
+#encoding "utf-8"  
+#GRAMMAR_ROOT S     
 
 
 Number -> AnyWord<wfl="[0-9]+[/,\\-]?[0-9]*">;
-ShortMes -> 'мл' | 'г' | 'гр' | 'кг' |'л' (Punct) ;
-ShortMesPart -> 'ч'| 'ст' (Punct);
-MeasurementGen -> (Adj<gnc-agr[1]>) Noun<gnc-agr[1], gram='gen', rt>;
-MeasurementNom -> (Adj<gnc-agr[1]>) Noun<gnc-agr[1], gram='nom', rt>;
+ShortMes -> 'мл' | 'г' | 'гр' | 'кг' |'л' ;
+ShortMesDot -> ShortMes ('.') ;
+
+ShortMesPart -> 'ч'| 'ст';
+ShortMesPartDot -> ShortMesPart ('.');
+
 AdjNoun -> (Adj<gnc-agr[1]>) Noun<gnc-agr[1], rt>;
-Mes -> (ShortMesPart) AdjNoun;
-Measurement -> Mes | ShortMes;
-Product -> AdjNoun;
-Ingredient -> Number interp (Ingredient.Quantity) (Measurement interp (Ingredient.Measurement)) Product interp (Ingredient.Product);
+MesPart -> (ShortMesPartDot) AdjNoun;
+Measurement -> MesPart | ShortMesDot;
+Ingredient -> Number interp (Ingredient.Quantity) (Measurement interp (Ingredient.Measurement)) AdjNoun interp (Ingredient.Product);
 
 
 V -> Word<gram="imper"> | Word<gram="inf"> | Verb<GU=['1p','pl']> ;
-AdjNounIns -> (Adj<gnc-agr[1]>) Noun<gnc-agr[1], gram='acc', rt>;
 Action -> V interp (Step.Action::norm='inf');
+
 AdjNounAcc -> (Adj<gnc-agr[1]>) Noun<gnc-agr[1], gram='acc', rt>;
-N ->  AdjNounAcc interp (Step.Object::norm='acc')| Ingredient interp (Step.Object::norm='acc');
-Object -> N interp (Step.Object::norm='acc');
+NP ->  AdjNounAcc interp (Step.Object::norm='acc')| Ingredient interp (Step.Object::norm='acc');
+Object -> NP interp (Step.Object::norm='acc');
+
 AO -> Action Object;
 OA -> Object Action;
 Step -> (Object) Action (Object);
@@ -30,4 +32,5 @@ Time -> Number Period;
 S -> Ingredient;
 S -> Step;
 S -> Step AnyWord* Time interp (Step.Time);
+
 
